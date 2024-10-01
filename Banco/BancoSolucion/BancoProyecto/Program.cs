962941876f6d3787d32abf4b13de3ce72fd42cd6
Adrian Jacek-- <adrian.jacek@tajamar365.com>
2024-10-01 21:12:19 +0200

    banco

---
+++ Banco/BancoSolucion/BancoProyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BancoProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ESCOGE UNA OPCION:\n" +
+                "1) Meter dinero en la cuenta\n" +
+                "2) Sacar dinero de la cuenta");
+        }
+    }
+}
